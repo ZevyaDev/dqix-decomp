07d6d2982d5407282a322c85e877a1b5005124aa
ZevyaDev <39806174+ZevyaDev@users.noreply.github.com>
2026-09-14 23:23:59 -0400

    Download mwasmarm alongside the compiler

--- tools/configure.py
+++ tools/configure.py
@@ -365,7 +365,7 @@ def add_download_tool_builds(n: ninja_syntax.Writer):
     if args.compiler is None:
         n.build(
             rule="download_tool",
-            outputs=[CC, LD],
+            outputs=[CC, LD, AS],
             variables={
                 "tool": "mwccarm",
                 "tag": "latest",
